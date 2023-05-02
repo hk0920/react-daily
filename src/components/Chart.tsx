3ea6b51e01f1ce8431a8b45f4392edbc82008051
hk0920 <gmlrb920@naver.com>
2023-05-02 18:03:27 +0900

    [add] chart - doughnut, radar - test

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,13 +1,16 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, Title } from "chart.js";
-import { Doughnut, Line } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, Title, Filler, RadialLinearScale } from "chart.js";
+import { Doughnut, Line, Radar } from "react-chartjs-2";
 import styled from "styled-components";
 
 // start - https://react-chartjs-2.js.org/
 // demo - https://react-chartjs-2.js.org/examples/line-chart
 
 // npm 명령어 - npm install --save chart.js react-chartjs-2
-ChartJS.register(ArcElement, Tooltip, Legend);
+// ChartJS.register(ArcElement, Tooltip, Legend);
 ChartJS.register(
+  ArcElement,
+  RadialLinearScale,
+  Filler,
   CategoryScale,
   LinearScale,
   PointElement,
@@ -18,26 +21,34 @@ ChartJS.register(
 );
 
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['Html5', 'Css', 'Sass', 'Jquery', 'JavaScript', 'React', 'TypeScript', 'StyledComponents', 'Git', 'GitHub'],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [40, 40, 20, 50, 30, 20, 10, 10, 40, 40],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
+        'rgba(227, 79, 38, 0.2)',
+        'rgba(21, 114, 182, 0.2)',
+        'rgba(204, 102, 153, 0.2)',
+        'rgba(21, 114, 182, 0.2)',
+        'rgba(247, 223, 30, 0.2)',
+        'rgba(97, 218, 251, 0.2)',
+        'rgba(21, 114, 182, 0.2)',
+        'rgba(204, 102, 153, 0.2)',
+        'rgba(240, 80, 20, 0.2)',
+        'rgba(24, 23, 23, 0.2)',
       ],
       borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
+        'rgba(227, 79, 38, 1)',
+        'rgba(21, 114, 182, 1)',
+        'rgba(204, 102, 153, 1)',
+        'rgba(21, 114, 182, 1)',
+        'rgba(247, 223, 30, 1)',
+        'rgba(97, 218, 251, 1)',
+        'rgba(21, 114, 182, 1)',
+        'rgba(204, 102, 153, 1)',
+        'rgba(240, 80, 20, 1)',
+        'rgba(24, 23, 23, 1)',
       ],
       borderWidth: 1,
     },
@@ -79,6 +90,26 @@ export const lineData = {
   ],
 };
 
+export const RadarData = {
+  labels: ['Html5', 'Css', 'Sass', 'Jquery', 'JavaScript', 'React', 'TypeScript', 'StyledComponents', 'Git', 'GitHub'],
+  datasets: [
+    {
+      label: 'Skill',
+      data: [90, 90, 70, 90, 60, 60, 70, 60, 80, 80],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+    },
+    {
+      label: 'Skill2',
+      data: [100, 50, 60, 40, 50, 20, 100, 100, 100, 100, 80],
+      backgroundColor: 'rgba(0, 99, 132, 0.2)',
+      borderColor: 'rgba(0, 99, 132, 1)',
+      borderWidth: 1,
+    },
+  ],
+}
+
 const SubTitle = styled.h4`
   font-size: 20px;
   margin-bottom: 20px;
@@ -100,6 +131,10 @@ export const Chart = () =>{
       <SubTitle>2. 선형차트</SubTitle>
       <BoxChart>
         <Line options={options} data={lineData} />
+      </BoxChart>
+      <SubTitle>3. 다각형 차트</SubTitle>
+      <BoxChart>
+        <Radar data={RadarData} />;
       </BoxChart>
     </>
   )
